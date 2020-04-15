ce12d2ea177928973226036c2666e6e9990ed8e0
Adam Smith <adam.smith@appvia.io>
2020-04-15 14:38:03 +0100

    build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -5,5 +5,5 @@ docker login -u="ukhomeofficedigital+acp_clamav" -p=${DOCKER_PASSWORD} quay.io
 
 for image in "clamav clamav-http clamav-notify"
 do
-  docker build --no-cache -t quay.io/ukhomeofficedigital/acp-$image:$DRONE_COMMIT_SHA $image
+  docker build --no-cache -t "quay.io/ukhomeofficedigital/acp-$image:$DRONE_COMMIT_SHA" "$image"
 done
